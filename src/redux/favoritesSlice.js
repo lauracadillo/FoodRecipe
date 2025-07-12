34e4fa14a8702f7dc6a945555a869688aaadb19b
lauracadillo <lauracadillo13@gmail.com>
2025-07-12 16:02:20 -0400

    favoriteSlice finished

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,13 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+  favoriterecipes: [], // Updated to handle favorite Recipes
 };
 
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
+    toggleFavorite: (state, action) => {
+        const recipe = action.payload; // Expecting the entire recipe object
+        const existingIndex = state.favoriteRecipes.findIndex(
+          (item) => item.id === recipe.idrecipe // Assuming Recipes have an 'id' property
+        );
+        if (existingIndex >= 0) {
+          state.favoriteRecipes.splice(existingIndex, 1); // Remove from favorites
+        } else {
+          state.favoriteRecipes.push(recipe); // Add to favorites
+        }
+      },
    
   },
 });
