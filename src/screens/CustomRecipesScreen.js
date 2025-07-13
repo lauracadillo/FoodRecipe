bcb71b64e9f66f334fdc4c530860788f242ae537
lauracadillo <lauracadillo13@gmail.com>
2025-07-13 16:38:59 -0400

    add recipe to custom recipe screen

--- src/screens/CustomRecipesScreen.js
+++ src/screens/CustomRecipesScreen.js
@@ -51,7 +51,7 @@ import {
         {/* Recipe Image */}
         <View style={styles.imageContainer} testID="imageContainer">
         {recipe.image && (
-            <Image source={{ uri: recipe.image }} style={styles.recipeImage} />
+            <Image source={{ uri: recipe.recipeImage }} style={styles.recipeImage} />
           )}
         </View>
         <View
@@ -73,10 +73,9 @@ import {
   
         {/* Recipe Details */}
         <View style={styles.contentContainer} testID="contentContainer">
-        <Text style={styles.recipeTitle}>{recipe.title}</Text>
+        <Text style={styles.recipeTitle}>{recipe.recipeTitle}</Text>
   <View style={styles.sectionContainer}>
     <Text style={styles.sectionTitle}>Content</Text>
-    <Text style={styles.contentText}>{recipe.description}</Text>
   </View>
         </View>
       </ScrollView>
